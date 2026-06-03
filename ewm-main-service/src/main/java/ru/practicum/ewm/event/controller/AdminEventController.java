91c799ef9cf74e0035ac1b19f883e7fbe8529e1c
aiymellaia <aiymdaurenkyzy@yandex.kz>
2026-06-03 11:32:36 +0500

    fix: checkstyle bug

--- ewm-main-service/src/main/java/ru/practicum/ewm/event/controller/AdminEventController.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/event/controller/AdminEventController.java
@@ -1,6 +1,5 @@
 package ru.practicum.ewm.event.controller;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
